434ae84475cecb830d1ac4ac8bffb122403aac95
Ayush yadav <ayushyaduvanshi79@gmail.com>
2025-05-13 11:12:39 +0530

    Updated portfolio with more information

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -72,9 +72,10 @@ const About = () => {
       </div>
 
       <p ref={paragraphRef} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
-        I'm a skilled software developer with experience in  JavaScript, and expertise in frameworks 
-        like React, Node.js, and Three.js. I'm a quick learner and collaborate closely with clients to create efficient, 
-        scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!
+        I'm a skilled software engineer with expertise in web development, specializing in creating dynamic
+        and responsive web applications. I have a strong foundation in JavaScript, React, and Node.js, and I'm passionate about
+        building user-friendly interfaces and optimizing performance. My goal is to leverage my skills to contribute to innovative projects
+        and deliver high-quality solutions that meet user needs.
       </p>
 
       <div className="mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center gap-10">
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,14 @@ const Footer = () => {
   return (
     <footer className="w-full py-4 bg-black-100/55 text-white flex justify-center gap-6">
       <a
-        href="https://www.instagram.com/huzaifahmedz/"
+        href="https://www.instagram.com/ayushyaduvanshi79/"
         target="_blank"
         rel="noopener noreferrer"
       >
         <FaInstagram size={24} />
       </a>
-      <a href="https://twitter.comhttps://x.com/HuzaifDev" target="_blank" rel="noopener noreferrer">
-        <FaXTwitter size={24} />
-      </a>
       <a
-        href="https://www.linkedin.com/in/huzaif-ahmed-6abb49273/"
+        href="https://www.linkedin.com/in/ayush-yadav-066711349?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
         target="_blank"
         rel="noopener noreferrer"
       >
